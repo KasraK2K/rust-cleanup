6c92b51285bce74e520e8e5c75c48c34ff5a22e4
Kasra Karami <Kasra_K2K@yahoo.com>
2026-01-08 17:40:17 +0330

    Add argument for folder name

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,14 @@
-use rust_cleanup::{get_path, clean_recursive_file, last_month_date};
+use rust_cleanup::{clean_recursive_file, get_path, last_month_date, resolve_path};
 use std::path::Path;
+use std::env;
 
 fn main() {
     let last_month = last_month_date();
 
-    let root = get_path();
+    let root = match env::args().nth(1) {
+        Some(arg) => resolve_path(&arg),
+        None => get_path(),
+    };
 
     clean_recursive_file(Path::new(&root), last_month, 10);
 }
